d74c6f989e7e539578bdae0bfe75e0fc15740f69
yihu-lv <1720427782@qq.com>
2021-04-26 16:05:19 +0800

    demo，update，ant引入

--- src/main.js
+++ src/main.js
@@ -1,7 +1,20 @@
 import Vue from 'vue'
+// 完整导入
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
 import App from './App.vue'
+// 局部引入
+// import { Button, message } from 'ant-design-vue';
+
 
 Vue.config.productionTip = false
+Vue.use(Antd);
+
+/* v1.1.3+ 自动注册Button下组件，如Button.Group */
+// Vue.use(Button);
+
+// Vue.prototype.$message = message;
+
 
 new Vue({
   render: h => h(App),
